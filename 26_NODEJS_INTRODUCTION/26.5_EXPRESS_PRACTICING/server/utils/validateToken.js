84f63c7737dc662a53cfb289e79785781d1804f4
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-26 15:10:40 -0300

    validateTOken updated w/ regex

--- 26_NODEJS_INTRODUCTION/26.5_EXPRESS_PRACTICING/server/utils/validateToken.js
+++ 26_NODEJS_INTRODUCTION/26.5_EXPRESS_PRACTICING/server/utils/validateToken.js
@@ -1,17 +1,8 @@
 export default function validateToken(token) {
   if (!token || token.length !== 12) return false;
 
-  const validChars = 'abcdefghijklmnopqrstuvwyxz0123456789';
+  const validCharsRegex = /^[\w|\d]*$/gi;
 
-  let valid = true;
-
-  token.split('').forEach((char) => {
-    const lowerChar = char.toLowerCase();
-
-    if (!validChars.includes(lowerChar)) {
-      valid = false;
-    }
-  });
-
-  return valid;
+  return validCharsRegex.test(token);
 }
+
