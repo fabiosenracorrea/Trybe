bcc19d43ac24b37f5431bacd5ca4dfc4f84ab0a1
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-25 21:45:42 -0300

    wrong module removed

--- 27_SOFTWARE_ARCHITECTURE_REST_RESTFULL/27.3_REST_RESTFUL/exercises/MySQL_server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import productRoutes from './product.routes.js';
-
-const routes = express.Router();
-
-routes.use('/products', productRoutes);
-
-export default routes;
